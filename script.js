ada2bd967b43b51956b551532f72e2e23eec68d6
jackgillies <jack.gillies@my.jcu.edu.au>
2017-05-26 15:57:17 +1000

    Add files via upload
    Completed site, fitted with 5 detailed artists pages, completed artists gallery and aftermath report

---
+++ script.js
@@ -0,0 +1,11 @@
+//file script.js
+
+function imgEnlarge(x) {
+    x.style.height = "180px";
+    x.style.width = "180px";
+}
+
+function imgNorm(x) {
+    x.style.height = "160px";
+    x.style.width = "120px";
+}
